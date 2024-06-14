8e76228e2dc9f201f568e20fe10b1f1cf70036d9
DilipGoud-03 <152593374+DilipGoud-03@users.noreply.github.com>
2024-06-14 16:41:29 +0530

    Delete jwt directory

--- jwt/api/db/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+pymysql://root@localhost:3306/grpc"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
